d84a23a57ddb5ee862e8a71ebb3d80d0f9a4c4e6
Ziad Zebib <zebibziad@gmail.com>
2020-12-30 00:38:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,8 +4,15 @@ def find_min_in_nested_arrays(src)
   while row_index < src.count do
     element_index = 0
     min_value = 1000
+<<<<<<< HEAD
     while element_index < src[row_index].count do 
       if min_value > src[row_index][element_index]
+=======
+    p "corgi #{row_index}"
+    p src[0].count
+    while element_index < src[row_index].count do 
+      if min_value > src[row_index][element_index] do
+>>>>>>> c1cc7b58df0ec6bc4b6dac54d6f3c25b8f7d4946
         min_value = src[row_index][element_index]
       end
       element_index += 1
@@ -15,3 +22,7 @@ def find_min_in_nested_arrays(src)
   end
   min_value_array
 end
+<<<<<<< HEAD
+=======
+end
+>>>>>>> c1cc7b58df0ec6bc4b6dac54d6f3c25b8f7d4946
